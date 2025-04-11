2856ca840c6671a04ad4430a2be6f0b5c31162fc
DuKeM-CSGO <1611098076@qq.com>
2025-04-11 23:12:49 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <print>
+#include <version>
+
+#if defined(_MSC_VER)
+    #define COMPILER_MSVC 1
+#elif defined(__clang__)
+    #define COMPILER_CLANG 1
+#elif defined(__GNUC__)
+    #define COMPILER_GCC 1
+#else
+    #define COMPILER_UNKNOWN 1
+#endif
+
+int main() {
+#if COMPILER_MSVC
+    //std::println("MSVC Version: {}", _MSC_VER);
+    std::println("Full Version: {}", _MSC_FULL_VER);
+#elif COMPILER_GCC
+    std::println("GCC Version: {}.{}.{}", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+    //std::println("Full Version: {}", __VERSION__);
+#elif COMPILER_CLANG
+    std::println("Clang Version: {}.{}.{}", __clang_major__, __clang_minor__, __clang_patchlevel__);
+    //std::println("Full Version: {}", __clang_version__);
+#else
+    std::println("Unknown Compiler";
+#endif
+
+    return 0;
+}
